ed490a092c537c623aa8b6eea97233acf4e1fa3d
Belkacem Alidra <dev@b-alidra.com>
2018-06-25 13:07:33 +0200

    Reset

--- day4/auth.js
+++
@@ -1,8 +0,0 @@
-/*
- * YABE - Authentication & Authorization
- */
-const passport = require("passport");
-
-module.exports = {
-    basic: () => passport.authenticate('basic', {session: false}),
-};
